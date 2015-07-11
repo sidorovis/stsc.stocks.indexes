42c3be8d64902e5de69ec78367c52e9028b6ad27
Ivan Sidarau <ivan.sidarau@gmail.com>
2015-07-11 09:03:54 +0200

    added MetaIndicesRepositoryIncodeTest.java

---
+++ src/test/java/stsc/stocks/repo/MetaIndicesRepositoryIncodeTest.java
@@ -0,0 +1,24 @@
+package stsc.stocks.repo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import stsc.stocks.indexes.CountryMarketIndex;
+import stsc.stocks.indexes.GlobalMarketIndex;
+import stsc.stocks.indexes.RegionMarketIndex;
+
+public class MetaIndicesRepositoryIncodeTest {
+
+	@Test
+	public void metaIndicesRepositoryIncodeTest() {
+		final MetaIndicesRepository repo = new MetaIndicesRepositoryIncodeImpl();
+		Assert.assertNotNull(repo.getCountryMarketIndices());
+		Assert.assertNotNull(repo.getGlobalMarketIndices());
+		Assert.assertNotNull(repo.getRegionMarketIndices());
+
+		Assert.assertEquals(CountryMarketIndex.values().length, repo.getCountryMarketIndices().size());
+		Assert.assertEquals(GlobalMarketIndex.values().length, repo.getGlobalMarketIndices().size());
+		Assert.assertEquals(RegionMarketIndex.values().length, repo.getRegionMarketIndices().size());
+	}
+
+}
